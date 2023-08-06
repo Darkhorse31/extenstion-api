d26b52585f40216ed1515816ae42615d25235eb2
PrateekPo <prateek kumar>
2023-08-06 16:05:13 +0530

    commit 2

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,33 @@
+// module.exports = {
+//   development: {
+//     client: 'mysql',
+//     connection: {
+//       host: 'localhost',
+//       user: 'root',
+//       password: 'qwerty@Q1',
+//       database: 'mysql',
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations',
+//       directory: './migrations',
+//     },
+//     seeds: {
+//       directory: './seeds',
+//     },
+//   },
+// };
+
 module.exports = {
   development: {
-    client: 'mysql',
+    client: 'pg',
     connection: {
-      host: 'localhost',
-      user: 'root',
-      password: 'qwerty@Q1',
-      database: 'mysql',
+      host: 'dpg-cj7mjq4l975s738o8bm0-a.oregon-postgres.render.com',
+      database: 'extapp',
+      user: 'prateek12',
+      password: 'WKzRWBoNOulcQmXPFtCufthwHcxTLwFV',
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
     migrations: {
       tableName: 'knex_migrations',
--- migrations/20230730122926_makestudenttable.js
+++ migrations/20230730122926_makestudenttable.js
@@ -1,19 +1,14 @@
-// exports.up = function(knex) {
-//     return knex.schema.createTable('your_table_name', function(table) {
-//       table.increments('uid').primary();
-//       table.text('name');
-//       table.text('contact');
-//       table.text('adhaar');
-//       table.text('address');
-//       table.mediumblob('photo1'); 
-//       table.mediumblob('photo2');
-//       table.mediumblob('photo3');
-//       table.mediumblob('photo4');
-//       table.mediumblob('photo5');
-//     });
-//   };
+exports.up = function(knex) {
+    return knex.schema.createTable('your_table_name', function(table) {
+      table.increments('uid').primary();
+      table.text('name');
+      table.text('contact');
+      table.text('adhaar');
+      table.text('address');
+    });
+  };
   
-//   exports.down = function(knex) {
-//     return knex.schema.dropTableIfExists('your_table_name');
-//   };
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('your_table_name');
+  };
   
